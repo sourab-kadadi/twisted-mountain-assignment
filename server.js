8f21fd594d63909911f70fcfd053c2f4701d7c43
Sourab2021 <88597457+Sourab2021@users.noreply.github.com>
2024-08-26 03:38:47 +0530

    resolved final build error "the path was not configured properly"

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist/twisted-mountain-assignment')));
+app.use(express.static(path.join(__dirname, 'dist/twisted-mountain-assignment/browser')));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'dist/twisted-mountain-assignment/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/twisted-mountain-assignment/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port or port 8080
